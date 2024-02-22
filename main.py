4c615363d91c43da73382dc79243919fedb577de
bangtx <bangtran.hha@gmail.com>
2024-02-22 21:27:51 +0700

    pmw at arduino

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from serial import Serial
 from time import sleep
 
-datas = [
-    "[24.12.2020 10:53:18] CO       5650ppm  SO2       978ppm  NO          0ppm  -----0       ---- -----0       ---- -----0       ---- O2      19.95%Vol NO2        10ppm  -----0       ---- -----0       ---- ",
-    "[24.12.2020 10:53:18] CO       5650ppm  SO2       978ppm  NO          0ppm  -----0       ---- -----0       ---- -----0       ---- O2      19.95%Vol NO2        10ppm  -----0       ---- -----0       ---- "
-]
+data = "[24.12.2020 10:53:18] CO       2000ppm  SO2       500ppm  NO       1000ppm  -----0       ---- -----0       ---- -----0       ---- O2      21.95%Vol NO2        10ppm  -----0       ---- -----0       ---- "
 
 ser = Serial('COM3', 115200, timeout=5)
 
 while True:
-    data = datas[0]
 # for data in datas:
     CO = data[29:35].strip()
     SO2 = data[47:53].strip()
@@ -17,7 +13,7 @@ while True:
     O2 = data[137:143].strip()
     NO2 = data[155:161].strip()
     # print(CO, SO2, NO, O2, NO2)
-    send_data = f'CO={CO} SO2={SO2}, NO={NO}, O2={O2}, NO2={NO2}'
+    send_data = f'CO={CO} SO2={SO2}, NO={NO}, _O2={O2}, NO2={NO2}'
     ser.write(send_data.encode())
     print(ser.readline().decode())
     # sleep(0.3)
--- voltage_to_current.ino
+++ voltage_to_current.ino
@@ -1,21 +1,51 @@
-String data = "CO=5650, SO2=978, NO=0, O2=19.95, NO2=10";
+String data = "CO=5650, SO2=978, NO=0, _O2=19.95, NO2=10";
 
 void setup() {
   Serial.begin(115200);
-  pinMode(13, OUTPUT);
+  for (int i = 2; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+  }
 }
 
+// CO, NO, dải là 0 đến 2000PPM, SO2 0 đến 1000ppm, NO2 là 0 đến 500ppm, O2 0 đến 22%
 void loop() {
   // Your code here
   delay(1000); // Delay for serial initialization
   if (Serial.available() > 0) {
     String send_data = Serial.readString();
-    int co2 = getValue(data, "CO=");
-    int so2 = getValue(data, "SO2=");
-    int no = getValue(data, "NO=");
-    int o2 = getValue(data, "O2=");
-    int no2 = getValue(data, "NO2=");
-    Serial.println("co2" + String(co2));
+    int co2 = getValue(send_data, "CO=");
+    int so2 = getValue(send_data, "SO2=");
+    int no = getValue(send_data, "NO=");
+    int o2 = getValue(send_data, "_O2=");
+    int no2 = getValue(send_data, "NO2=");
+
+    int pmw_co2 = map(co2, 0, 2000, 0, 255);
+    int pmw_so2 = map(so2, 0, 1000, 0, 255);
+    int pmw_no = map(no, 0, 2000, 0, 255);
+    int pmw_o2 = map(o2, 0, 22, 0, 255);
+    int pmw_no2 = map(no2, 0, 500, 0, 255);
+    Serial.println(
+      "pmw_co2=" + String(pmw_co2) +
+      "pmw_so2=" + String(pmw_so2) +
+      "pmw_co2=" + String(pmw_no) +
+      "pmw_no=" + String(pmw_co2) +
+      "pmw_o2=" + String(pmw_o2) +
+      "pmw_no2=" + String(pmw_no2)
+    );
+//    Serial.println(
+//      "co2=" + String(co2) +
+//      "so2=" + String(so2) +
+//      "co2=" + String(no) +
+//      "no=" + String(co2) +
+//      "o2=" + String(o2) +
+//      "no2=" + String(no2)
+//    );
+    analogWrite(2, pmw_co2);
+    analogWrite(3, pmw_so2);
+    analogWrite(4, pmw_no);
+    analogWrite(5, pmw_co2);
+    analogWrite(6, pmw_o2);
+    analogWrite(7, pmw_no2);
   }
 //  Serial.println(data);
 
@@ -29,13 +59,13 @@ int getValue(String data, String parameter) {
     if (endIndex == -1) { // If comma not found, consider until the end of string
       endIndex = data.length();
     }
-    // Serial.println(startIndex);
-    // Serial.println(endIndex);
+//    Serial.println(startIndex);
+//    Serial.println(endIndex);
     String valueString = data.substring(startIndex, endIndex);
+//    Serial.println(valueString);
     int value = valueString.toInt(); // Convert string to float
     // Serial.print(parameter);
     // Serial.println(value);
     return value;
   }
 }
-
